5db9edfa49df3ad2270aa8755533407fbf8662ec
thongxuan <thongxuan@users.noreply.github.com>
2026-03-06 05:55:05 +0700

    ui: gateway sheet 340, language sheet 480

--- KuromiApp/Views/SetupView.swift
+++ KuromiApp/Views/SetupView.swift
@@ -82,7 +82,7 @@ struct SetupView: View {
         // Gateway sheet
         .sheet(isPresented: $showGatewaySheet) {
             GatewaySheet(gatewayURL: $viewModel.gatewayURL, gatewayToken: $viewModel.gatewayToken)
-                .presentationDetents([.height(480)])
+                .presentationDetents([.height(340)])
                 .presentationDragIndicator(.visible)
                 .presentationBackground(Color.appSheetBackground)
                 .presentationBackgroundInteraction(.disabled)
