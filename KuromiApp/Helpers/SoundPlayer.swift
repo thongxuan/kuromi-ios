6b558064fd790cdb63d37815b1beaa5e023d661e
thongxuan <thongxuan@users.noreply.github.com>
2026-03-06 21:51:32 +0700

    fix: start=1111, stop=1110

--- KuromiApp/Helpers/SoundPlayer.swift
+++ KuromiApp/Helpers/SoundPlayer.swift
@@ -2,7 +2,7 @@ import AudioToolbox
 
 enum SoundPlayer {
     static func playStart(completion: (() -> Void)? = nil) {
-        AudioServicesPlaySystemSoundWithCompletion(1110) { completion?() }
+        AudioServicesPlaySystemSoundWithCompletion(1111) { completion?() }
     }
 
     static func playStop(completion: (() -> Void)? = nil) {
